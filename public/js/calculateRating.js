e1aea9f91603b5055500c90acecc505aa097b420
Antonio Gloria <angloria87@gmail.com>
2022-11-23 16:24:08 -0600

    Now properly displaying recipe rating in detail and multi views

--- public/js/calculateRating.js
+++ public/js/calculateRating.js
@@ -2,14 +2,7 @@ const starEmptySrc = "/../images/ui/ratings/starEmpty.svg";
 const starHalfSrc = "/../images/ui/ratings/starHalf.svg";
 const starFullSrc = "/../images/ui/ratings/starFull.svg";
 
-const ratingContainer = document.querySelector(".rating");
-const starsContainer = document.querySelector(".stars-container");
-
-const ratingsData = ratingContainer.getAttribute("values").split(",");
-
-const ratings = ratingsData.map(rating => {
-  return Number(rating);
-});
+const ratingContainers = document.querySelectorAll(".rating");
 
 function average(valuesArr) {
   let sumAll = valuesArr.reduce((total, current) =>{
@@ -18,27 +11,36 @@ function average(valuesArr) {
   return sumAll/valuesArr.length;
 }
 
-const score = average(ratings).toFixed(1);
-const scoreInt = Math.floor(score);
-const scoreDec = (score - scoreInt).toFixed(1);
+ratingContainers.forEach(container => {
+  const starsContainer = container.querySelector(".stars-container");
+  const ratingsData = container.getAttribute("values").split(",");
 
-let half = false; //If the half star has been added
+  const ratings = ratingsData.map(rating => {
+    return Number(rating);
+  });
 
-for (let i = 0; i < 5; i++) {
-  const newImg = document.createElement('img');
+  const score = average(ratings).toFixed(1);
+  const scoreInt = Math.floor(score);
+  const scoreDec = (score - scoreInt).toFixed(1);
 
-  if (i < scoreInt) {
-    newImg.setAttribute("src", starFullSrc);
-  }
-  else {
-    if (!half && scoreDec >= 0.5) {
-      newImg.setAttribute("src", starHalfSrc);
-      half = true;
+  let half = false; //If the half star has been added
+
+  for (let i = 0; i < 5; i++) {
+    const newImg = document.createElement('img');
+
+    if (i < scoreInt) {
+      newImg.setAttribute("src", starFullSrc);
     }
     else {
-      newImg.setAttribute("src", starEmptySrc);
+      if (!half && scoreDec >= 0.5) {
+        newImg.setAttribute("src", starHalfSrc);
+        half = true;
+      }
+      else {
+        newImg.setAttribute("src", starEmptySrc);
+      }
     }
-  }
 
-  starsContainer.appendChild(newImg);
-}
+    starsContainer.appendChild(newImg);
+  }
+});

43a6c7b747dfa86859ef8e917af04aef4d5b7ce4
Antonio Gloria <angloria87@gmail.com>
2023-08-31 15:16:30 -0600

    Added NotFound.svg and included it in not-found.hbs

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,42 +9,50 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 /* GET home page */
 router.get("/", async (req, res, next) => {
   try {
-    const latestRecipes = await Recipe.find()
-                                      .sort({"createdAt": -1})
-                                      .limit(4)
-                                      .populate("author")
+    const latestRecipes = await Recipe
+      .find()
+      .sort({"createdAt": -1})
+      .limit(4)
+      .populate("author")
 
     res.render("index", { latestRecipes });
   }
   catch (err) {
-    console.log(err)
+    next(err)
   }
 });
 
 router.get("/:username", async (req, res, next) => {
   try {
     const { username } = req.params;
-    const userData = await User.findOne( {username: username} ).populate("createdRecipes")
-                                                                .populate({
-                                                                  path: 'createdRecipes',
-                                                                  populate: {
-                                                                    path: 'author',
-                                                                    model: 'User'
-                                                                    }
-                                                                  })
-                                                               .populate("favoriteRecipes")
-                                                               .populate({
-                                                                path: 'favoriteRecipes',
-                                                                populate: {
-                                                                  path: 'author',
-                                                                  model: 'User'
-                                                                  }
-                                                                })
+    const userData = await User
+      .findOne( {username: username} )
+      .populate("createdRecipes")
+      .populate({
+        path: 'createdRecipes',
+        populate: {
+          path: 'author',
+          model: 'User'
+          }
+        })
+      .populate("favoriteRecipes")
+      .populate({
+      path: 'favoriteRecipes',
+      populate: {
+        path: 'author',
+        model: 'User'
+        }
+      })
 
-    res.render("userProfile", userData);
+    if (!userData) {
+      res.status(404).render("not-found");
+    }
+    else {
+      res.render("userProfile", userData);
+    }
   }
   catch (err) {
-    console.log(err)
+    next(err)
   }
 });
 
